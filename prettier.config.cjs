18b6351487bc47e7b04380e8ecf3053409fe1e35
Antti Kivi <antti.kivi@visiosto.fi>
2023-12-17 15:37:09 +0200

    build: add Prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('prettier').Config} */
+const config = {
+  plugins: [
+    'prettier-plugin-packagejson',
+    'prettier-plugin-organize-imports',
+  ],
+  arrowParens: 'always',
+  bracketSpacing: true,
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+  proseWrap: 'always',
+};
+
+export default config;
+
